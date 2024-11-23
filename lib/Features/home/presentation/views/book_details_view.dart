9c945a3df6deb1b6673fced23f3af8b10184c0ea
HadyAbdallah <ahady916@gmail.com>
2024-11-23 17:53:27 +0200

    Ui Set Up

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'widgets/book_details_view_body.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: BookDetailsViewBody(),
+      ),
+    );
+  }
+}
