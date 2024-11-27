c50027ad84c1f98ed98bff0362136b106f7691f9
HadyAbdallah <ahady916@gmail.com>
2024-11-27 17:53:21 +0200

    Register book entity adapter and open featured box and Cache featured books

--- lib/Features/home/data/data_source/home_remote_data_source_impl.dart
+++ lib/Features/home/data/data_source/home_remote_data_source_impl.dart
@@ -1,7 +1,11 @@
 import 'package:bookly_clear_arch/Features/home/data/data_source/home_remote_data_source.dart';
 import 'package:bookly_clear_arch/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_clear_arch/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_clear_arch/constants.dart';
 import 'package:bookly_clear_arch/core/utils/api_service.dart';
+import 'package:hive/hive.dart';
+
+import '../../../../core/utils/functions/save_books.dart';
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   final ApiService apiService;
@@ -20,6 +24,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&q=Programming&Sorting=newest');
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books, kFeaturedBooks);
     return books;
   }
 
